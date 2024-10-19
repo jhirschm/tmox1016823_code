8e6712ab526a10c081f6febb915853137c97c789
jhirschm <jhirschm@stanford.edu>
2024-10-19 10:53:09 -0700

    plot

--- noise_check.py
+++ noise_check.py
@@ -9,6 +9,7 @@ run = next(ds.runs())
 ch = 22
 evt = next(run.events())
 evt = next(run.events())
+
 hsd = run.Detector('mrco_hsd')
 
 channels = [0, 22, 45, 67, 90, 112, 135, 157, 180, 202, 225, 247, 270, 292, 315, 337]
@@ -21,7 +22,7 @@ for i, ch in enumerate(channels):
     row = i // 4  # Determine row in 4x4 grid
     col = i % 4   # Determine column in 4x4 grid
 
-    y = hsd.raw.peaks(evt)[ch][0][1][0]  # Extract the peak data
+    evt = next(run.events())
     while True:
         print("1")
         try:
@@ -30,11 +31,10 @@ for i, ch in enumerate(channels):
             w = hsd.raw.waveforms(evt)[ch][0][1][0]  # Extract the waveform data
         except:
             evt = next(run.events())
-            evt = next(run.events())
 
         else:
             break
-    
+    y = hsd.raw.peaks(evt)[ch][0][1][0]  # Extract the peak data
     # Plot on respective subplot
     axs[row, col].plot(y.astype(float)/2+float(1<<13), label="peak")
     # axs[row, col].plot(w*(1<<3), label="wave")
