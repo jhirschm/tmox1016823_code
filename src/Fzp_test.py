871121fa5f6c17062e4854eaab43944ce286447c
jhirschm <jhirschm@stanford.edu>
2024-10-19 16:20:41 -0700

    plot

---
+++ src/Fzp_test.py
@@ -0,0 +1,13 @@
+import psana
+import numpy as np
+import sys
+import re
+import h5py
+import os
+import socket
+
+from typing import Type,List
+
+from Fzp import *
+
+print("Hello from Fzp_test.py")
