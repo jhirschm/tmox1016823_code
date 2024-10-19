312100a33e116f822e041a35d8fccc0873abd116
jhirschm <jhirschm@stanford.edu>
2024-10-19 10:17:37 -0700

    plot

--- noise_check.py
+++ noise_check.py
@@ -1,1 +1,20 @@
 import psana
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+ds = psana.DataSource(exp='tmoc00123',run=22)
+
+run = next(ds.runs())
+ch = 22
+evt = next(run.events())
+evt = next(run.events())
+hsd = next(run.Detector('mrco_hsd'))
+y = hsd.raw.peaks(evt)[ch][0][1][0]
+w =  hsd.raw.waveforms(evt)[ch][0][1][0]
+
+plt.figure()
+plt.plot(w*(1<<3),label="wave")
+plt.plot(y.astype(float)/2+float(1<<13),label="peak")
+plt.legend()
+plt.show()
