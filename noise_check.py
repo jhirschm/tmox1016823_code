5009c477083fc5361e7d41b8b7675968e2c50008
jhirschm <jhirschm@stanford.edu>
2024-10-19 10:46:38 -0700

    plot

--- noise_check.py
+++ noise_check.py
@@ -22,18 +22,18 @@ for i, ch in enumerate(channels):
     col = i % 4   # Determine column in 4x4 grid
 
     y = hsd.raw.peaks(evt)[ch][0][1][0]  # Extract the peak data
-    while True:
-        try:
-            w = hsd.raw.waveforms(evt)[ch][0][1][0]  # Extract the waveform data
-            print("here")
-        except:
-            evt = next(run.events())
-        else:
-            break
+    # while True:
+    #     try:
+    #         w = hsd.raw.waveforms(evt)[ch][0][1][0]  # Extract the waveform data
+    #         print("here")
+    #     except:
+    #         evt = next(run.events())
+    #     else:
+    #         break
     
     # Plot on respective subplot
     axs[row, col].plot(y.astype(float)/2+float(1<<13), label="peak")
-    axs[row, col].plot(w*(1<<3), label="wave")
+    # axs[row, col].plot(w*(1<<3), label="wave")
     
     # Set subtitle indicating both the channel and label
     axs[row, col].set_title(f'Channel {ch}, Label {labels[i]}')
