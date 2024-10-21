416458164cfeb13bab3716ecb885e8af949315dd
jhirschm <jhirschm@stanford.edu>
2024-10-21 11:59:43 -0700

    plot

--- src/bias_characterization_fex.py
+++ src/bias_characterization_fex.py
@@ -14,7 +14,10 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # Set up argument parser to take show_fourier as a command-line argument
 parser = argparse.ArgumentParser(description='Experiment.')
 parser.add_argument('--exp-name', type=str, help='Name of the experiment.')
-parser.add_argument('--load-path', type=str, default=None, help='Path to load data (default: None).')
+parser.add_argument('--max-load-path', type=str, default=None, help='Path to load data (default: None).')
+parser.add_argument('--hist-load-path', type=str, default=None, help='Path to load data (default: None).')
+parser.add_argument('--bins-load-path', type=str, default=None, help='Path to load data (default: None).')
+
 parser.add_argument('--save-plot-path', type=str, default=None, help='Path to load data (default: None).')
 
 
@@ -27,6 +30,19 @@ args = parser.parse_args()
 exp_name = args.exp_name
 load_path = args.load_path
 save_plot_path = args.save_plot_path
+max_load_path = args.max_load_path
+hist_load_path = args.hist_load_path
+bins_load_path = args.bins_load_path
+
+save = False
+
+
+if bins_load_path is not None:
+    mcp_bias = np.load(bins_load_path)
+else:
+    mcp_bias = np.array([1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800])
+
+
 
 
 
@@ -38,7 +54,6 @@ channels = [0, 22, 45, 67, 90, 112, 135, 157, 180, 202, 225, 247, 270, 292, 315,
 labels = list(range(17))  # Labeled as 0 through 16
 
 runs_list = [17, 16, 15, 14 ,5,6,7,8,9, 10,11, 12,13]
-mcp_bias = np.array([1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800])
 # ds = psana.DataSource(exp=exp_name, run=7)
 # run = next(ds.runs())
 # hsd = run.Detector('mrco_hsd')
@@ -82,43 +97,53 @@ binvals = np.arange(0,(1<<15)+1,1<<6)
 
 num_runs = len(runs_list)
 num_channels = len(channels)
-histograms = np.zeros((num_runs, num_channels, len(binvals) - 1))
-channel_max_values = np.zeros((len(runs_list),len(channels)))
+if hist_load_path is not None:
+    histograms = np.load(hist_load_path)
+else:
+    histograms = np.zeros((num_runs, num_channels, len(binvals) - 1))
 
+if max_load_path is not None:
+    channel_max_values = np.load(max_load_path)
+    print(f"NumPy array loaded from {load_path}")
+else:
+    channel_max_values = np.zeros((len(runs_list),len(channels)))
 
-for i, run_num in enumerate(runs_list):
-    ds = psana.DataSource(exp=exp_name, run=run_num)
-    run = next(ds.runs())
-    hsd = run.Detector('mrco_hsd')
+if max_load_path is None and hist_load_path is None:
+    for i, run_num in enumerate(runs_list):
+        ds = psana.DataSource(exp=exp_name, run=run_num)
+        run = next(ds.runs())
+        hsd = run.Detector('mrco_hsd')
 
-    # Loop through all events in the run
-    for num, evt in enumerate(run.events()):
-        peaks = hsd.raw.peaks(evt)
+        # Loop through all events in the run
+        for num, evt in enumerate(run.events()):
+            peaks = hsd.raw.peaks(evt)
 
-        for j, chan in enumerate(channels):
-            
+            for j, chan in enumerate(channels):
+                
+                                
+                if peaks is not None:
+                    for k in range(len(peaks[chan][0][1])):
+                        if len(peaks[chan][0][1]) > 2:
+                            max_value = peaks[chan][0][1][k].max()  # Get the maximum value for the peak
                             
-            if peaks is not None:
-                for k in range(len(peaks[chan][0][1])):
-                    if len(peaks[chan][0][1]) > 2:
-                        max_value = peaks[chan][0][1][k].max()  # Get the maximum value for the peak
-                        
-                        # Find the appropriate bin index for the single value
-                        bin_index = np.digitize(max_value, binvals) - 1  # Subtract 1 to convert to zero-based index
+                            # Find the appropriate bin index for the single value
+                            bin_index = np.digitize(max_value, binvals) - 1  # Subtract 1 to convert to zero-based index
 
-                        # Ensure the bin index is within the valid range
-                        if 0 <= bin_index < len(histograms[i, j]):
-                            histograms[i, j, bin_index] += 1  # Increment the count in the appropriate bin
-                        else:
-                            print(f"Value out of range for histogram bins for run {run_num}, channel {chan}: {max_value}")
-                        if max_value > channel_max_values[i,j]:
-                            channel_max_values[i,j] = max_value
+                            # Ensure the bin index is within the valid range
+                            if 0 <= bin_index < len(histograms[i, j]):
+                                histograms[i, j, bin_index] += 1  # Increment the count in the appropriate bin
+                            else:
+                                print(f"Value out of range for histogram bins for run {run_num}, channel {chan}: {max_value}")
+                            if max_value > channel_max_values[i,j]:
+                                channel_max_values[i,j] = max_value
 
-        if num >= 10000:
-            break
-np.save("histograms5.npy", histograms)
-np.save("binvals5.npy", binvals)
-np.save("channel_max_values_fex5.npy", channel_max_values)
+            if num >= 10000:
+                break
+
+if save:
+    np.save("histograms6.npy", histograms)
+    np.save("binvals6.npy", binvals)
+    np.save("channel_max_values_fex6.npy", channel_max_values)
 
 
 colors = plt.cm.get_cmap('tab10', len(channels)).colors
@@ -199,8 +224,8 @@ bias_text = "\n".join([f"Ch {chan}: Bias={bias:.2f}" for chan, bias in zip(chann
 plt.figtext(0.78, 0.5, bias_text, fontsize=10, ha='left', va='center', bbox=dict(facecolor='white', edgecolor='black'))
 
 # Show the plot
-plt.savefig("channel_max_values_plot_biasLine_labeled_thresh5.png")
-print("channel_max_values_plot_biasLine_thresh5.png")
+# plt.savefig("channel_max_values_plot_biasLine_labeled_thresh5.png")
+print("channel_max_values_plot_biasLine_thresh6.png")
 plt.show()
 
 # At this point, `histograms` contains the 3D histogram counts
@@ -209,6 +234,31 @@ for run_idx in range(num_runs):
     for chan_idx in range(num_channels):
         print(f"Run {runs_list[run_idx]}, Channel {channels[chan_idx]} Histogram Counts: {histograms[run_idx, chan_idx]}")
 
+for i, chan in enumerate(channels):
+    fig, axs = plt.subplots(nrows=len(mcp_bias), ncols=1, figsize=(10, 18), sharex=True)
+
+    for j in range(len(mcp_bias)):
+        # Plot each histogram with legends
+        axs[j].bar(binvals[300:-1], histograms[j, chan, 300:], width=np.diff(binvals[300:]), align='edge', edgecolor='black', alpha=0.7)
+
+        # Set y-axis limits
+        axs[j].set_ylim(0, 60)
+
+        # Add legends for MCP Bias instead of subtitles
+        axs[j].legend([f'MCP Bias: {mcp_bias[j]}'], loc='upper right', fontsize=10)
+
+    # Set the main title for the entire plot based on channel number
+    fig.suptitle(f'Channel {chan} - FEX Max Pulse Height per Window', fontsize=16)
+
+    # Set the x-label and y-label for the last subplot
+    axs[-1].set_xlabel('FEX Max Pulse Height per Window', fontsize=12)
+    axs[0].set_ylabel('Counts', fontsize=12)
+
+    # Adjust layout for better spacing
+    fig.tight_layout(rect=[0, 0, 1, 0.95])  # Leave space for the main title
+
+    # Show the plot
+    plt.show()
 
 # fig, axs = plt.subplots(nrows=len(mcp_bias), ncols=2, figsize=(10, 18), sharex=True)
 # for (j,chan) in enumerate(channels):
