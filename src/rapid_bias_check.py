0b81f591ae9bd3d91c9249c54c5ab7f1fdeb9d40
jhirschm <jhirschm@stanford.edu>
2024-10-23 15:20:05 -0700

    hist

--- src/rapid_bias_check.py
+++ src/rapid_bias_check.py
@@ -19,6 +19,7 @@ parser.add_argument('--save-folder-path', type=str, default=None, help='Path to 
 
 # Add argument for either a single run or a list of runs
 parser.add_argument('--run', type=int, nargs='+', help='Single run or a list of runs.')
+parser.add_argument('--mcp-bias', type=int, nargs='+', default=None, help='MCP biases for run.')
 
 # Add argument for maximum number of events
 parser.add_argument('--max-events', type=int, help='Maximum number of events to process.')
@@ -47,6 +48,12 @@ channels = args.channels
 channel_polarity = args.channel_polarity
 runs = args.run
 secondary_hist_load_path = args.wrapped_hist_load_path
+
+# Validate MCP bias length
+if args.mcp_bias is not None:
+    if len(args.mcp_bias) != len(runs):
+        raise ValueError(f'The length of MCP biases ({len(args.mcp_bias)}) must match the number of runs ({len(runs)}).')
+
 
 
 
@@ -128,23 +135,17 @@ if hist_load_path is None:
                             # Secondary histogram logic based on thresholds
                             if polarity == 1:
                                 # For positive polarity, look for max value below threshold
-                                secondary_signal_value = peaks[chan][0][1][k][0:pos_threshold_bin_index].max()
-                                if secondary_signal_value <= args.pos_threshold:
-                                    secondary_bin_index = np.digitize(secondary_signal_value, binvals) - 1
+                                indices = np.where((peaks[chan][0][1][k] < args.pos_threshold) & (peaks[chan][0][1][k] >= 0))
+                                filtered_values = peaks[chan][0][1][k][indices]
+                                max_value = np.max(filtered_values) if filtered_values.size > 0 else None
+                                # print(max_value)
+                                if max_value is not None:
+                                    secondary_bin_index = np.digitize(max_value, binvals) - 1
                                     if 0 <= secondary_bin_index < len(secondary_histograms[i, j]):
                                         secondary_histograms[i, j, secondary_bin_index] += 1  # Increment in secondary histogram
-                                    else:
-                                        print(f"Value out of range for secondary histogram bins for run {run_num}, channel {chan}: {secondary_signal_value}")
-
                             elif polarity == -1:
                                 # For negative polarity, look for min value above threshold
-                                secondary_signal_value = peaks[chan][0][1][k][neg_threshold_bin_index:].min()
-                                if secondary_signal_value >= args.neg_threshold:
-                                    secondary_bin_index = np.digitize(secondary_signal_value, binvals) - 1
-                                    if 0 <= secondary_bin_index < len(secondary_histograms[i, j]):
-                                        secondary_histograms[i, j, secondary_bin_index] += 1  # Increment in secondary histogram
-                                    else:
-                                        print(f"Value out of range for secondary histogram bins for run {run_num}, channel {chan}: {secondary_signal_value}")
+                                print("not implemented")
 
             if num >= max_num_events:
                 break
@@ -159,29 +160,37 @@ num_rows = min(16, num_channels)
 
 # Create the PDF file for saving the plots
 with PdfPages(f'{save_folder_path}/experiment_plots.pdf') as pdf:
-    
+    width_ratio = len(binvals[250:-1]) / len(binvals[0:pos_threshold_bin_index])
+
     if num_runs == 1:
         # Single run case: All channels on the same page
         # fig, axs = plt.subplots(num_rows, 2, figsize=(10, num_rows * 3))
-        fig, axs = plt.subplots(nrows=num_rows, ncols=1, figsize=(10, 18), sharex=True)
+        fig, axs = plt.subplots(nrows=len(runs), ncols=2, figsize=(10, 24),
+                                gridspec_kw={'width_ratios': [1, width_ratio]}, sharex=False)
 
-        fig.suptitle(f'Run {runs[0]}', fontsize=16)
+        if args.mcp_bias is not None:
+            fig.suptitle(f'Run {runs[0]} (MCP Bias: {args.mcp_bias[0]})', fontsize=16)
+        else:
+            fig.suptitle(f'Run {runs[0]}', fontsize=16)
         
-        max_value =  60#np.max(histograms[0,:, 300:])  # Get the maximum value for setting the y-axis limits
-        max_secondary_value = 10#np.max(secondary_histograms[0,:, 0:100])  # Get the maximum value for secondary histograms
+        max_value_1 =np.max(secondary_histograms[0, :,:])#np.max(histograms[:,:, 300:])  # Get the maximum value for setting the y-axis limits
+        max_value_2 =np.max(histograms[0, :,300:])
         for j, chan in enumerate(channels):
             row = j // 2
-            
-            # Plot secondary histograms on the left column
-            axs[j, 0].bar(binvals[0:100], secondary_histograms[0, j, 0:100], width=np.diff(binvals[0:100+1]), edgecolor="black")
-            axs[j, 0].set_title(f'Channel {chan} (Secondary)', fontsize=12)
-            axs[j, 0].set_ylim(0, max_value * 1.1)  # Set ylim to max value with a 10% margin
+            axs[j, 0].bar(binvals[0:pos_threshold_bin_index], secondary_histograms[0, j,0:pos_threshold_bin_index], width=np.diff(binvals[0:pos_threshold_bin_index+1]), edgecolor="red", facecolor="red")
+            axs[j, 0].set_title(f'Channel {chan} (Rollover)', fontsize=12)
+            axs[j, 0].set_ylim(0, max_value_1 * 1.1)  # Set ylim to max value with a 10% margin
 
             # Plot primary histograms on the right column
-            axs[j, 1].bar(binvals[300:-1], histograms[0, j,  300:], width=np.diff(binvals[300:]), edgecolor="black")
+            axs[j, 1].bar(binvals[250:-1], histograms[i, j,250:], width=np.diff(binvals[250:]), edgecolor="black", facecolor="black")
             axs[j, 1].set_title(f'Channel {chan} (Primary)', fontsize=12)
-            axs[j, 1].set_ylim(0, max_secondary_value * 1.1)  # Set ylim to max value with a 10% margin
-
+            axs[j, 1].set_ylim(0, max_value_2 * 1.1)  # Set ylim to max value with a 10% margin
+        #Set the x-label and y-label for the last subplot
+        axs[-1, 0].set_xlabel(f'FEX Max Pulse Height <{pos_threshold_bin_index} per Window', fontsize=12)
+        axs[-1, 1].set_xlabel('FEX Max Pulse Height per Window', fontsize=12)
+        
+        # Adjust layout for better spacing with your specific settings
+        plt.subplots_adjust(top=0.92, bottom=0.05, left=0.125, right=0.9, hspace=0.7, wspace=0.6)
         plt.tight_layout(rect=[0, 0, 1, 0.96])
         pdf.savefig(fig)  # Save the current figure to the PDF
         plt.close(fig)  # Close the figure to free memory
@@ -189,32 +198,38 @@ with PdfPages(f'{save_folder_path}/experiment_plots.pdf') as pdf:
     else:
         # Multiple runs case: One channel per page, all runs on the same page for that channel
         for j, chan in enumerate(channels):
-            # fig, axs = plt.subplots(num_runs, 2, figsize=(10, num_runs * 3))
-            fig, axs = plt.subplots(nrows=num_rows, ncols=1, figsize=(10, 18), sharex=True)
+            # Set up the figure with variable column widths
+            fig, axs = plt.subplots(nrows=len(runs), ncols=2, figsize=(10, 24),
+                                    gridspec_kw={'width_ratios': [1, width_ratio]}, sharex=False)
 
             fig.suptitle(f'Channel {chan}', fontsize=16)
-
-            max_value = 60#np.max(histograms[:,:, 300:])  # Get the maximum value for setting the y-axis limits
-            max_secondary_value = np.max(secondary_histograms[:,:, 0:100])  # Get the maximum value for secondary histograms
+            max_value_1 =np.max(secondary_histograms[:, j,:])#np.max(histograms[:,:, 300:])  # Get the maximum value for setting the y-axis limits
+            max_value_2 =np.max(histograms[:, j,300:])
             for i, run_num in enumerate(runs):
-                # Plot secondary histograms for this run on the left column
-                # axs[i, 0].bar(binvals[0:100], secondary_histograms[i, j, 0:100], width=np.diff(binvals[0:100+1]), edgecolor="black")
-                # axs[i, 0].set_title(f'Run {run_num} (Secondary)', fontsize=12)
-                # axs[i, 0].set_ylim(0, max_secondary_value * 1.1)  # Set ylim to max value with a 10% margin
-
                 # Plot primary histograms for this run on the right column
-                axs[i, 0].bar(binvals[300:-1], histograms[i, j, 300:], width=np.diff(binvals[300:]), edgecolor="black")
-                axs[i, 0].set_title(f'Run {run_num} (Primary)', fontsize=12)
-                axs[i, 0].set_ylim(0, max_value * 1.1)  # Set ylim to max value with a 10% margin
-                axs[i].set_ylabel('Counts', fontsize=12)
+                axs[i, 0].bar(binvals[0:pos_threshold_bin_index], secondary_histograms[i, j,0:pos_threshold_bin_index], width=np.diff(binvals[0:pos_threshold_bin_index+1]), edgecolor="red", facecolor="red")
+                if args.mcp_bias is not None:
+                    axs[i, 0].set_title(f'Run {run_num} (Roll Over) MCP Bias: {args.mcp_bias[i]}', fontsize=12)
+                else:
+                    axs[i, 0].set_title(f'Run {run_num} (Roll Over)', fontsize=12)
+                axs[i, 0].set_ylim(0, max_value_1*1.1)  # Set ylim to max value with a 10% margin
+                axs[i, 0].set_ylabel('Counts', fontsize=12)
+                
+                # Plot primary histograms for this run on the right column
+                axs[i, 1].bar(binvals[250:-1], histograms[i, j,250:], width=np.diff(binvals[250:]), edgecolor="black", facecolor="black")
+                axs[i, 1].set_title(f'Run {run_num} (Primary)', fontsize=12)
+                axs[i, 1].set_ylim(0, max_value_2 * 1.1)  # Set ylim to max value with a 10% margin
             # Set the x-label and y-label for the last subplot
-            axs[-1].set_xlabel('FEX Max Pulse Height per Window', fontsize=12)
+            axs[-1, 0].set_xlabel(f'FEX Max Pulse Height <{pos_threshold_bin_index} per Window', fontsize=12)
+            axs[-1, 1].set_xlabel('FEX Max Pulse Height per Window', fontsize=12)
         
             # Adjust layout for better spacing with your specific settings
             plt.subplots_adjust(top=0.92, bottom=0.05, left=0.125, right=0.9, hspace=0.7, wspace=0.6)
             plt.tight_layout(rect=[0, 0, 1, 0.96])
             pdf.savefig(fig)  # Save the current figure to the PDF
             plt.close(fig)  # Close the figure to free memory
+
+
 
 
 
